5bc2e562d0e740c2570bd5a2284d304b87a0ea15
Kidist-Abraham <kidistabraham@gmail.com>
2019-08-25 00:57:45 +0300

    Add a feature (find all occurrence of the word)

--- crawler.js
+++ crawler.js
@@ -2,7 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
-var indexs;
+var indexs=[];
 var page = "https://nixos.org/nix/manual/";
 console.log(" page " + page);
 request(page, function(error, response, body) {
@@ -18,9 +18,18 @@ request(page, function(error, response, body) {
 
 function searchWord($, word) {
   var bodyText = $('html > body').text();
-  if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
-   var ind = bodyText.toLowerCase().indexOf(word.toLowerCase())
-    return bodyText.substring(ind - 7, ind + 40);
+  var bodyLower=bodyText.toLowerCase()
+  var wordLower=word.toLowerCase()
+  var start =0
+  var ind;
+  if(bodyLower.indexOf(wordLower) !== -1) {
+   while (bodyLower.indexOf(wordLower,start) !== -1){
+   ind = bodyLower.indexOf(wordLower,start)
+   indexs.push(bodyText.substring(ind - 10, ind + 10)) 
+   start=ind+1
+   
+}
+    return indexs;
   }
   return false;
 }
