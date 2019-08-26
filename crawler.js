ac3387e2c5342df3b02d71ed3878e388492c9281
Kidist-Abraham <kidistabraham@gmail.com>
2019-08-26 10:52:37 +0300

    format code

--- crawler.js
+++ crawler.js
@@ -9,29 +9,32 @@ var word = "--dump"
 console.log("The page: " + MainPage);
 console.log("The word: " + word);
 searchMain(MainPage)
+var $;
 function searchMain(page) {
 
-var relativeLinks = [];
-request(page, function(error, response, body) {
-   if(error) {
-     return("Error: " + error);
-   }
-  // console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
+  var relativeLinks = [];
+  request(page, function(error, response, body) {
+    if(error) {
+      return("Error: " + error);
+    }
+    // console.log("Status code: " + response.statusCode);
+    if(response.statusCode === 200) {
 
-     var $ = cheerio.load(body);
-     $("a[href^='/']").each(function() {
-      relativeLinks.push($(this).attr('href'));
- });
-     console.log("Search result on page "+page + " : ")
-     console.log(searchWord($, word,relativeLinks,page))
-    
-   }
+      $ = cheerio.load(body);
+    //  console.log(typeof($));
+      $("a[href^='/']").each(function() {
+        relativeLinks.push($(this).attr('href'));
+      });
+      console.log("Search result on page "+page + " : ")
+      console.log(searchWord($, word,relativeLinks,page))
+
+    }
 
 
-//console.log(relativeLinks) 
-});
-return indexs
+    //console.log(relativeLinks)
+  });
+ 
+  return indexs
 }
 
 function searchWord($, word,relativeLinks,page) {
@@ -42,38 +45,33 @@ function searchWord($, word,relativeLinks,page) {
   var start =0
   var ind;
   if(bodyLower.indexOf(wordLower) !== -1) {
-   while (bodyLower.indexOf(wordLower,start) !== -1){
-   ind = bodyLower.indexOf(wordLower,start)
-   indexs.push(bodyText.substring(ind - (word.length+10), ind + (word.length+10))) 
-   start=ind+1
-   
-}
+    while (bodyLower.indexOf(wordLower,start) !== -1){
+      ind = bodyLower.indexOf(wordLower,start)
+      indexs.push(bodyText.substring(ind - (word.length+10), ind + (word.length+10)))
+      start=ind+1
+
+    }
     return indexs;
   }
-else{
- 
-  if (relativeLinks.length>1){
-   console.log(word + " can't be found in the page "+page+". Searching for subpages");
-var ind =0
- while(ind<relativeLinks.length){
-  if(relativeLinks[ind]!="/"){
-   searchMain(page+relativeLinks[ind])
-   ind=ind+1
-}
-else ind=ind+1
-}
- // (relativeLinks.splice(1)).forEach(function(el){
- //  searchMain(page+el)
-//})
-}
-else {
-  return (word + " can't be found in the page "+page)
-}
-}
+  else{
+
+    if (relativeLinks.length>1){
+      console.log(word + " can't be found in the page "+page+". Searching for subpages");
+      var ind =0
+      while(ind<relativeLinks.length){
+        if(relativeLinks[ind]!="/"){
+          searchMain(page+relativeLinks[ind])
+          ind=ind+1
+        }
+        else ind=ind+1
+      }
+      // (relativeLinks.splice(1)).forEach(function(el){
+      //  searchMain(page+el)
+      //})
+    }
+    else {
+      return (word + " can't be found in the page "+page)
+    }
+  }
 
 }
-
-
-
-
-
